e59a17b48c1318802a27dee8689ed9f2fb29237f
Celve <celve03@gmail.com>
2022-09-22 23:57:38 +0800

    ci(frontend): automatically add package identifier

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -13,9 +13,24 @@ dependencies {
 tasks.generateGrammarSource {
   maxHeapSize = "64m"
   arguments = arguments + listOf("-visitor", "-long-messages")
-  sync {
+
+  // copy generated files to source
+  copy {
     from("${buildDir}/generated-src/antlr/main")
     into("src/main/java/parser")
   }
-  delete("${buildDir}/generated-src/antlr/main/*")
+
+//   insert package into the first line, only applicable in macOS
+  exec {
+    workingDir = File("src/main/java/parser")
+
+//  on macOS
+    commandLine("./package.sh")
+//    commandLine("ex -sc '1i|package parser' -cx 'MeteorParserBaseListener.java'")
+//    commandLine("ex -sc '1i|package parser' -cx 'MeteorParserBaseVisitor.java'")
+//    commandLine("ex -sc '1i|package parser' -cx 'MeteorParserLexer.java'")
+//    commandLine("ex -sc '1i|package parser' -cx 'MeteorParserListener.java'")
+//    commandLine("ex -sc '1i|package parser' -cx 'MeteorParserParser.java'")
+//    commandLine("ex -sc '1i|package parser' -cx 'main/java/parser/MeteorParserVisitor.java'")
+  }
 }
