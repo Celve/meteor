01b93a6cb7b38cdb0c389e1bbcbe0f46cdc0378b
Celve <celve03@gmail.com>
2022-12-14 22:06:23 +0800

    fix(ConstPropagator): fix add all executable successors when adding a block

--- src/main/kotlin/middleend/pass/ConstPropagator.kt
+++ src/main/kotlin/middleend/pass/ConstPropagator.kt
@@ -65,7 +65,7 @@ object ConstPropagator : IRVisitor() {
   private fun addBlock2WorkList(block: BasicBlock) {
     if (!blockStateMap.getValue(block)) {
       blockWorkList.add(block)
-      blockWorkList.addAll(block.nextBlockList.filter { blockStateMap.getValue(block) })
+      blockWorkList.addAll(block.nextBlockList.filter { blockStateMap.getValue(it) })
       blockStateMap[block] = true
     }
   }
